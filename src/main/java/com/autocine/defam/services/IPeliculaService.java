ce8b9047304f161da75b038ee8863d8d82b0b045
diegoRMS02 <90098490+diegoRMS02@users.noreply.github.com>
2024-10-15 01:16:05 -0500

    Revert "Revert "peliculas""

---
+++ src/main/java/com/autocine/defam/services/IPeliculaService.java
@@ -0,0 +1,17 @@
+package com.autocine.defam.services;
+
+import java.util.List;
+
+import com.autocine.defam.model.Pelicula;
+
+public interface IPeliculaService {
+    void agregarPelicula(Pelicula pelicula);
+
+    List<Pelicula> obtenerTodasLasPeliculas();
+
+    Pelicula obtenerPeliculaPorTitulo(String titulo);
+
+    void eliminarPelicula(String titulo);
+
+    List<Pelicula> obtenerEstrenos();
+}
