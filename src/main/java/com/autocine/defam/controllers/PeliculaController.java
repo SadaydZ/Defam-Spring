f0f7908ae263f00b5887ff23b8073d4922e525d4
diegoRMS02 <90098490+diegoRMS02@users.noreply.github.com>
2024-10-15 01:13:46 -0500

    Revert "peliculas"

--- src/main/java/com/autocine/defam/controllers/PeliculaController.java
+++ src/main/java/com/autocine/defam/controllers/PeliculaController.java
@@ -1,36 +1,37 @@
 package com.autocine.defam.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.autocine.defam.model.Pelicula;
-import com.autocine.defam.services.IPeliculaService;
+import com.autocine.defam.model.Pelicula; // Importa la clase Pelicula
 
 @Controller
-@RequestMapping("/peliculas")
 public class PeliculaController {
 
-    @Autowired
-    private IPeliculaService peliculaService;
+    @GetMapping("/peliculas")
+    public String peliculas(Model model) {
+        // lista de películas para recomendaciones
+        List<Pelicula> recomendaciones = new ArrayList<>();
+        recomendaciones.add(new Pelicula("Avengers: Endgame", "/images/foto.png"));
+        recomendaciones.add(new Pelicula("The Lion King", "/images/foto.png"));
+        recomendaciones.add(new Pelicula("Toy Story 4", "/images/foto.png"));
+        recomendaciones.add(new Pelicula("Joker", "/images/foto.png"));
 
-    @GetMapping("/listar")
-    public String listarPeliculas(Model model) {
-        List<Pelicula> listaPeliculas = peliculaService.obtenerTodasLasPeliculas();
-        List<Pelicula> listaEstrenos = peliculaService.obtenerEstrenos();
-        model.addAttribute("peliculas", listaPeliculas);
-        model.addAttribute("estrenos", listaEstrenos);
-        return "peliculas"; // nombre de la vista (listarPeliculas.html)
-    }
+        // lista de próximos estrenos
+        List<Pelicula> proximosEstrenos = new ArrayList<>();
+        proximosEstrenos.add(new Pelicula("Dune", "/images/foto.png"));
+        proximosEstrenos.add(new Pelicula("No Time to Die", "/images/foto.png"));
+        proximosEstrenos.add(new Pelicula("The Matrix 4", "/images/foto.png"));
+        proximosEstrenos.add(new Pelicula("West Side Story", "/images/foto.png"));
 
-    @PostMapping("/agregar")
-    public String agregarPelicula(Pelicula pelicula) {
-        peliculaService.agregarPelicula(pelicula);
-        return "redirect:/peliculas/listar";
+        // listas al modelo
+        model.addAttribute("recomendaciones", recomendaciones);
+        model.addAttribute("proximosEstrenos", proximosEstrenos);
+
+        return "peliculas"; //
     }
 }
